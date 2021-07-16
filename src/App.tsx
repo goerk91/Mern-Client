8b32720b518cc11b67c23e695d38c9ca9e7163d8
Metin Görken <metin.goerken@adesso.de>
2021-07-16 09:29:03 +0200

    Updated main branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-// new branch development
+// updated Main branch
 function App() {
   return (
     <div className="App">
