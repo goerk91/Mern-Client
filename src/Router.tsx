b655c6294b7b1946d0a853d649f386c4879fffa2
Metin Görken <metin.goerken@adesso.de>
2021-07-16 10:32:35 +0200

    Creating Router.tsx

---
+++ src/Router.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import {BrowserRouter, Switch, Route} from "react-router-dom" 
+
+export default function Router() {
+    return (
+        <BrowserRouter>
+        </BrowserRouter>
+    )
+}
