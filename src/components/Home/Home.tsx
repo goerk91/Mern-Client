6425ef810f64b5b70bb432e39cbcef06ccc9c95d
Metin Görken <metin.goerken@adesso.de>
2021-07-20 23:16:33 +0200

    Worked on Home

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./home.css";
+
+export default function Home() {
+  return (
+    <div className="animated-title">
+      <div className="text-top">
+        <div>
+          <span>Front and Backend</span>
+          <span>Development</span>
+        </div>
+      </div>
+      <div className="text-bottom">
+        <div>React, Node.js, MongodDb and Express</div>
+      </div>
+    </div>
+  );
+}
