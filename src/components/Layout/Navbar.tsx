beea986c062a922b7cf31916f5bf24d9b585dc85
Metin Görken <metin.goerken@adesso.de>
2021-07-20 23:15:56 +0200

    Structured divs

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -2,22 +2,31 @@ import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import AuthContext from "../../context/AuthContext";
 import LogoutBtn from "../auth/LogoutBtn";
+import "./navbar.css";
 
 export default function Navbar() {
   const { loggedIn } = useContext(AuthContext);
 
   return (
-    <div>
-      <Link to="/">Home</Link>
+    <div className="nav-container">
+      <div className="menu">
+        <Link to="/">Home</Link>
+      </div>
       {loggedIn === false && (
         <>
-          <Link to="/register">Register</Link>
-          <Link to="/login">Log in</Link>
+          <div className="menu">
+            <Link to="/register">Register</Link>
+          </div>
+          <div className="menu">
+            <Link to="/login">Log in</Link>
+          </div>
         </>
       )}
       {loggedIn === true && (
         <>
-          <Link to="/customer">Customer</Link>
+          <div className="menu">
+            <Link to="/customer">Customer</Link>
+          </div>
           <LogoutBtn />
         </>
       )}
