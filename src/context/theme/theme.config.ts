0474ae2ca305ab2980493a319523e56362a60faf
Metin Görken <metin.goerken@adesso.de>
2021-07-26 08:38:56 +0200

    theme config

---
+++ src/context/theme/theme.config.ts
@@ -0,0 +1,10 @@
+import { ThemeType, Theme } from "./ThemeType";
+
+export const THEME: Record<ThemeType, Theme> = {
+  light: {
+    backgroundColor: "red",
+  },
+  dark: {
+    backgroundColor: "yellow",
+  },
+};
